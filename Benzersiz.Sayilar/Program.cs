8ab43efb9294a571787d1e7f1d145fec901a4c8d
AlisefaAkkas <76962859+AlisefaAkkas@users.noreply.github.com>
2021-03-08 23:55:06 +0300

    Add project files.

---
+++ Benzersiz.Sayilar/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benzersiz.Sayilar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kaç adet sayı üretelim?");
+            int adet = int.Parse(Console.ReadLine());
+            int[] sayi = new int[adet];
+            Random rnd = new Random();
+            for (int i = 0; i > sayi.Length; i++)
+            {
+
+                sayi[i] = rnd.Next(1, 100);
+            }
+
+            Console.WriteLine("Dizi :");
+            for (int i = 0; i < sayi.Length; i++)
+            {
+                Console.WriteLine($"{sayi[i]}-");
+                sayi[i] = rnd.Next(1, 100);
+
+
+
+            }
+            for (int i = 0; i < sayi.Length; i++)
+            {
+                Console.WriteLine($"{sayi[i]}-");
+            }
+
+
+            Array.Sort(sayi);
+            Console.WriteLine("Küçükten büyüğe sıralama :");
+            for (int i = 0; i < sayi.Length; i++)
+            {
+                Console.Write($"{sayi[i]}-");
+            }
+            Console.ReadKey();
+        }
+    }
+}
